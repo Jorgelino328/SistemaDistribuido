f41020fc25a82383f559349a3ad09237f968b404
Jorge <jorgewilliam328@gmail.com>
2025-04-29 03:26:39 -0300

    feat: add suport for multiple instances

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -20,11 +20,24 @@ public class Main {
     public static void main(String[] args) {
         SystemConfig.getInstance();
         
+        // Valores padrão
+        String componentType = "gateway";
+        int instanceNumber = 1;
         
         // Analisa os argumentos da linha de comando
-        String componentType = "gateway";
         if (args.length > 0) {
             componentType = args[0].toLowerCase();
+        }
+        
+        // Verifica se um número de instância foi especificado
+        if (args.length > 1) {
+            try {
+                instanceNumber = Integer.parseInt(args[1]);
+            } catch (NumberFormatException e) {
+                LOGGER.warning("Número de instância inválido: " + args[1]);
+                printUsage();
+                System.exit(1);
+            }
         }
         
         // Inicia o componente com base no tipo
@@ -33,10 +46,10 @@ public class Main {
                 startGateway();
                 break;
             case "componenta":
-                startComponentA();
+                startComponentA(instanceNumber);
                 break;
             case "componentb":
-                startComponentB();
+                startComponentB(instanceNumber);
                 break;
             default:
                 LOGGER.warning("Tipo de componente desconhecido: " + componentType);
@@ -63,17 +76,28 @@ public class Main {
     
     /**
      * Inicia uma instância do Componente A.
+     * 
+     * @param instanceNumber Número da instância a iniciar (1 ou 2)
      */
-    private static void startComponentA() {
-        LOGGER.info("Iniciando o Componente A...");
+    private static void startComponentA(int instanceNumber) {
+        LOGGER.info("Iniciando o Componente A (instância " + instanceNumber + ")...");
         
         SystemConfig config = SystemConfig.getInstance();
         String host = "localhost";
-        int httpPort = config.getComponentAHttpPort();
-        int tcpPort = config.getComponentATcpPort();
-        int udpPort = config.getComponentAUdpPort();
         String gatewayHost = config.getGatewayHost();
         int gatewayRegistrationPort = config.getRegistrationPort();
+        
+        int httpPort, tcpPort, udpPort;
+        
+        if (instanceNumber == 1) {
+            httpPort = config.getIntProperty("componentA.http.port", 8181);
+            tcpPort = config.getIntProperty("componentA.tcp.port", 8182);
+            udpPort = config.getIntProperty("componentA.udp.port", 8183);
+        } else {
+            httpPort = config.getIntProperty("componentA.http.port." + instanceNumber, 8191);
+            tcpPort = config.getIntProperty("componentA.tcp.port." + instanceNumber, 8192);
+            udpPort = config.getIntProperty("componentA.udp.port." + instanceNumber, 8193);
+        }
         
         ComponentA component = new ComponentA(
             host, httpPort, tcpPort, udpPort, gatewayHost, gatewayRegistrationPort
@@ -89,17 +113,28 @@ public class Main {
     
     /**
      * Inicia uma instância do Componente B.
+     * 
+     * @param instanceNumber Número da instância a iniciar (1 ou 2)
      */
-    private static void startComponentB() {
-        LOGGER.info("Iniciando o Componente B...");
+    private static void startComponentB(int instanceNumber) {
+        LOGGER.info("Iniciando o Componente B (instância " + instanceNumber + ")...");
         
         SystemConfig config = SystemConfig.getInstance();
         String host = "localhost";
-        int httpPort = config.getComponentBHttpPort();
-        int tcpPort = config.getComponentBTcpPort();
-        int udpPort = config.getComponentBUdpPort();
         String gatewayHost = config.getGatewayHost();
         int gatewayRegistrationPort = config.getRegistrationPort();
+        
+        int httpPort, tcpPort, udpPort;
+        
+        if (instanceNumber == 1) {
+            httpPort = config.getIntProperty("componentB.http.port", 8281);
+            tcpPort = config.getIntProperty("componentB.tcp.port", 8282);
+            udpPort = config.getIntProperty("componentB.udp.port", 8283);
+        } else {
+            httpPort = config.getIntProperty("componentB.http.port." + instanceNumber, 8291);
+            tcpPort = config.getIntProperty("componentB.tcp.port." + instanceNumber, 8292);
+            udpPort = config.getIntProperty("componentB.udp.port." + instanceNumber, 8293);
+        }
         
         ComponentB component = new ComponentB(
             host, httpPort, tcpPort, udpPort, gatewayHost, gatewayRegistrationPort
@@ -139,10 +174,13 @@ public class Main {
      * Imprime as instruções de uso.
      */
     private static void printUsage() {
-        System.out.println("Uso: java -jar sistema-distribuido.jar [tipoComponente]");
+        System.out.println("Uso: java -jar sistema-distribuido.jar [tipoComponente] [numeroInstancia]");
         System.out.println("  onde tipoComponente é um dos seguintes:");
         System.out.println("    gateway     - Inicia o Gateway de API");
         System.out.println("    componentA  - Inicia uma instância do Componente A");
         System.out.println("    componentB  - Inicia uma instância do Componente B");
+        System.out.println("  numeroInstancia é opcional (padrão: 1):");
+        System.out.println("    1          - Primeira instância do componente");
+        System.out.println("    2          - Segunda instância do componente");
     }
 }
